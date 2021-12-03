9204c4e4d00b08a792efb6a04c0684430f49c3e6
Michael Christenson <mchristenson5891@gmail.com>
2021-12-03 13:41:12 -0800

    ready to deploy

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -5,36 +5,33 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from 'firebase/auth';
-// Add your Firebase credentials
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyAqiUeRHou4YZl43pMyR-XVoNekeHuYT1U',
-  authDomain: 'hr-mvp-c9bab.firebaseapp.com',
-  projectId: 'hr-mvp-c9bab',
-  storageBucket: 'hr-mvp-c9bab.appspot.com',
-  messagingSenderId: '419677262435',
-  appId: '1:419677262435:web:b0dcd387e98ed0e93e1e56',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PORJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
 const auth = getAuth();
 
 const authContext = createContext();
-// Provider component that wraps your app and makes auth object ...
-// ... available to any child component that calls useAuth().
+
 export function ProvideAuth({ children }) {
   const auth = useProvideAuth();
   return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
-// Hook for child components to get the auth object ...
-// ... and re-render when it changes.
+
 export const useAuth = () => {
   return useContext(authContext);
 };
-// Provider hook that creates auth object and handles state
+
 function useProvideAuth() {
   const [user, setUser] = useState(null);
-  // Wrap any Firebase methods we want to use making sure ...
-  // ... to save the user to state.
+
   const signin = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password).then(
       (response) => {
@@ -70,11 +67,8 @@ function useProvideAuth() {
     return auth.confirmPasswordReset(code, password).then(() => {
       return true;
     });
-  };
-  // Subscribe to user on mount
-  // Because this sets state in the callback it will cause any ...
-  // ... component that utilizes this hook to re-render with the ...
-  // ... latest auth object.
+
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
@@ -83,10 +77,10 @@ function useProvideAuth() {
         setUser(false);
       }
     });
-    // Cleanup subscription on unmount
+
     return () => unsubscribe();
   }, []);
-  // Return the user object and auth methods
+
   return {
     user,
     signin,
